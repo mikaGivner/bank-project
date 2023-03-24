cfe08df0649369f286eb3d2642c5a0a75228f138
mika <mikagivner123@gmail.com>
2023-03-25 01:52:20 +0300

    port5000

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get("/", (req, res) =>
   res.status(200).json({ message: "server is running" })
 );
 app.use("/api/v1/newUser", newUser);
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
